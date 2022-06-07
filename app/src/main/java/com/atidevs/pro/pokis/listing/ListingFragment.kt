5b7f29061a2a0bdaeda77cf228050d01fd762798
Atmane TAOUSSI <atmane.taoussi1@ibm.com>
2022-06-07 20:42:47 +0100

    [REFACTOR][PART1] Refactor initial listing implementation to a new fragment with data binding

---
+++ app/src/main/java/com/atidevs/pro/pokis/listing/ListingFragment.kt
@@ -0,0 +1,27 @@
+package com.atidevs.pro.pokis.listing
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.atidevs.pro.pokis.databinding.FragmentListingBinding
+
+class ListingFragment : Fragment() {
+
+    private var _binding: FragmentListingBinding? = null
+    val binding: FragmentListingBinding = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListingBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
