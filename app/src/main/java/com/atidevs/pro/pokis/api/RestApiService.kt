37b997d0068b8d20e3d1c9b5233497e25fa1ea43
Atmane TAOUSSI <atmane.taoussi1@ibm.com>
2022-06-13 13:05:11 +0100

    [REFACTOR] Refactor and split models into network and data layers

--- app/src/main/java/com/atidevs/pro/pokis/api/RestApiService.kt
+++ app/src/main/java/com/atidevs/pro/pokis/api/RestApiService.kt
@@ -1,6 +1,5 @@
 package com.atidevs.pro.pokis.api
 
-import com.atidevs.pro.pokis.models.PokiResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -8,5 +7,8 @@ import retrofit2.http.Query
 interface RestApiService {
 
     @GET("/api/v2/pokemon/")
-    fun getPokemons(@Query("offset") offset: Int, @Query("limit") pageSize: Int): Call<PokiResponse>
+    fun getPokemons(
+        @Query("offset") offset: Int,
+        @Query("limit") pageSize: Int
+    ): Call<NetworkPagedResponse>
 }
---
+++ app/src/main/java/com/atidevs/pro/pokis/listing/Poki.kt
@@ -0,0 +1,26 @@
+package com.atidevs.pro.pokis.listing
+
+/**
+ * Data representation of a Pokémon
+ * @param name: of the pokémon
+ * @param url: for retrieving more details about the pokémon
+ */
+
+data class Poki(
+    val name: String = "",
+    val url: String = ""
+) {
+    override fun equals(other: Any?): Boolean {
+        return if (other != null && other is Poki) {
+            this.hashCode() == other.hashCode()
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + url.hashCode()
+        return result
+    }
+}
